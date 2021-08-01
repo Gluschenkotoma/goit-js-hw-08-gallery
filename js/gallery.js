a02d3a97f1587723f7997b88f3b54dc8a9b309d7
Tamara Glushchenko <gluschenkotoma@ukr.net>
2021-08-01 19:23:01 +0300

    8

--- js/gallery.js
+++ js/gallery.js
@@ -16,12 +16,15 @@ function galleryCardMarkup(images) {
       return `<li class="gallery__item">
             <a class="gallery__link"
             href=${original}>
-            <img class="gallery__image"
-            src=${preview}
+            <img
+            
+            loading = "lazy"
+            class="gallery__image lazyload"
+            data-src=${preview}
             data-source=${original}
-                          alt=${description} />
-                          </a>
-                    </li>`;
+            alt=${description} />
+            </a>
+            </li>`;
     })
     .join("");
 }
@@ -125,4 +128,31 @@ function onArrowsSwitchImg(event) {
     modalImg.setAttribute("alt", `${images[nextIndex].description}`);
     return modalImg;
   }
+}
+
+// feature detection
+if ("loading" in HTMLImageElement.prototype) {
+  addSrcAttrToLazyImages();
+} else {
+  addLazySizesScript();
+}
+
+const lazyImages = document.querySelectorAll("img[data-src]");
+
+function addSrcAttrToLazyImages() {
+  const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+  lazyImages.forEach((img) => {
+    img.src = img.dataset.src;
+  });
+}
+
+function addLazySizesScript() {
+  const script = document.createElement("script");
+  script.src =
+    "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+  script.integrity =
+    "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
+  script.crossorigin = "anonymous";
+  script.referrerpolicy = "no-referrer";
+  document.body.appendChild(script);
 }
